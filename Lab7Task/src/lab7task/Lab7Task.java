48fc6c8a6a35888aaf9d452e48c9298f87ec14e1
AbdulAzeem-Syed <syed.abd@northeastern.edu>
2023-11-15 19:24:15 -0500

    Project creation

---
+++ Lab7Task/src/lab7task/Lab7Task.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab7task;
+
+/**
+ *
+ * @author Abdul
+ */
+public class Lab7Task {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
